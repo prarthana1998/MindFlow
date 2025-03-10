edb488a06f9e19a4703bd6bd667fb30d70bbe071
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-03-10 22:30:58 +0000

    added comments in middleware code

--- middleware.js
+++ middleware.js
@@ -2,17 +2,17 @@ import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
-export default clerkMiddleware(async(auth, req)=>{
-  const { userId, redirectToSignIn } = await auth()
-  console.log('Middleware triggered for route:', req.url);
-  if(!userId && isProtectedRoute(req)) {
-    console.log('Redirecting to sign-in');
-    // Add logic to run if the user does not have the required permissions
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
 
-    return redirectToSignIn()
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
   }
-  return NextResponse.next()
+
+  return NextResponse.next();
 });
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
