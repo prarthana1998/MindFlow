bf2522314ec32b6f9861a0bf193b369e47ebd17a
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-03-10 22:37:27 +0000

    added comments in middleware code

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 export default clerkMiddleware(async(auth, req)=>{
   const { userId, redirectToSignIn } = await auth()
   console.log('Middleware triggered for route:', req.url);
